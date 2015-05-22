4f2a09a346a2328b15464c393668b9d6a7b5bc47
abouthiroppy <git@about-hiroppy.com>
2015-05-22 09:05:48 +0900

    add .emacs.d

---
+++ .emacs.d/elisp/stdafx.h
@@ -0,0 +1,29 @@
+/*
+stdafx.h -
+ */
+#ifndef CLANG_STDAFX_H_
+#define CLANG_STDAFX_H_
+//STL
+#include <algorithm>
+#include <bitset>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <climits>
+#include <typeinfo>
+#include <utility>
+#include <vector>
+/* #include <numeric> */
+//windows
+#include <windows.h>
+
+#endif //STDAFX_H_
---
+++ .emacs.d/elpa/elscreen-20120413.1107/elscreen-autoloads.el
@@ -0,0 +1,28 @@
+;;; elscreen-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (elscreen-start) "elscreen" "elscreen.el" (20740
+;;;;;;  2379))
+;;; Generated autoloads from elscreen.el
+
+(autoload 'elscreen-start "elscreen" "\
+
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("elscreen-pkg.el") (20740 2379 707412))
+
+;;;***
+
+(provide 'elscreen-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; elscreen-autoloads.el ends here
