a0223b198ca8ae90eefac4fd3382d1c09a1a1c75
Yuta Hiroto <git@about-hiroppy.com>
2020-03-18 20:15:26 +0900

    Add fish_prompt.fish

---
+++ config/fish/functions/fish_prompt.fish
@@ -0,0 +1,25 @@
+function fish_prompt
+  set last_status $status
+
+  if [ $status -eq 0 ]
+    set_color 69DC85
+    echo -n '(๑˃̵ᴗ˂̵)و'
+    set_color normal
+  else
+    set_color FF0087
+    echo -n '｡◕ˇ_ˇ◕｡'
+    set_color normal
+  end
+
+  echo -n ' '
+
+  set_color $fish_color_cwd
+  printf '%s' (prompt_pwd)
+  set_color normal
+
+  printf '%s ' (__fish_git_prompt)
+
+  set_color 69DC85
+  echo -n ' ᐅ '
+  set_color normal
+end
