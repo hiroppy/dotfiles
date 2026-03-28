a4315ccd82b73813de59b493957353d28e6b7a0d
Yuta Hiroto <git@hiroppy.me>
2026-03-28 09:21:16 +0900

    Fix env parser

--- config/fish/functions/envsource.fish
+++ config/fish/functions/envsource.fish
@@ -1,7 +1,8 @@
 function envsource
-  for line in (cat $argv | grep -v '^#')
+  for line in (cat $argv | grep -v '^#' | grep -v '^\s*$')
     set item (string split -m 1 '=' $line)
-    set -gx $item[1] $item[2]
-    echo "Exported key $item[1]"
+    set key (string replace 'export ' '' $item[1])
+    set -gx $key $item[2]
+    echo "Exported key $key"
   end
 end
