e3257370ca9a77b5aa071093ec6c815272ef6a57
Yuta Hiroto <git@hiroppy.me>
2026-07-14 15:30:26 +0900

    Revert color palette

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,6 +1,37 @@
 # delete the greeting message
 set fish_greeting
 
+# prompt colors
+set -g fish_color_autosuggestion 555 brblack
+set -g fish_color_cancel -- -r
+set -g fish_color_command f5f5f5
+set -g fish_color_comment 990000
+set -g fish_color_cwd 7AD5FE
+set -g fish_color_cwd_root red
+set -g fish_color_end 009900
+set -g fish_color_error ff9248
+set -g fish_color_escape 00a6b2
+set -g fish_color_history_current --bold
+set -g fish_color_host normal
+set -g fish_color_host_remote yellow
+set -g fish_color_match --background=brblue
+set -g fish_color_normal normal
+set -g fish_color_operator 00a6b2
+set -g fish_color_param 7AD5FE
+set -g fish_color_quote 999900
+set -g fish_color_redirection 00afff
+set -g fish_color_search_match white --background=brblack
+set -g fish_color_selection white --bold --background=brblack
+set -g fish_color_status red
+set -g fish_color_user brgreen
+set -g fish_color_valid_path --underline
+
+set -g fish_pager_color_completion
+set -g fish_pager_color_description B3A06D yellow
+set -g fish_pager_color_prefix white --bold --underline
+set -g fish_pager_color_progress brwhite --background=cyan
+set -g fish_pager_color_selected_background -- -r
+
 # locale
 set -gx LANG ja_JP.UTF-8
 
